cbe4b029e1d5875262692a9abb0002185025aef6
naftis <pyry.rouvila@futurice.com>
2019-01-16 01:05:28 +0200

    Add UI enablers for changing profile picture

--- src/containers/Profile/Profile.js
+++ src/containers/Profile/Profile.js
@@ -1,16 +1,22 @@
+import PropTypes from 'prop-types';
 import React from 'react';
-import { TouchableOpacity, StyleSheet, View } from 'react-native';
+import { StyleSheet, TouchableOpacity, View } from 'react-native';
+import FastImage from 'react-native-fast-image';
+import ImagePicker from 'react-native-image-picker';
 import { Navigation } from 'react-native-navigation';
 import { getProfileImageUrl } from '../../services/api';
 import Pictures from './Pictures';
-import PropTypes from 'prop-types';
-import FastImage from 'react-native-fast-image';
+import colors from '../../theme/colors';
 
 const SIDEMENU_ID = 'sideMenu';
 
 class Profile extends React.Component {
   static propTypes = {
     componentId: PropTypes.string.isRequired
+  };
+
+  state = {
+    profileImageSource: undefined
   };
 
   isDrawerOpen = false;
@@ -50,12 +56,45 @@ class Profile extends React.Component {
     });
   }
 
-  _changeProfilePicture() {
-    alert('TODO: Change profile picture');
+  async componentDidMount() {
+    const profileImageUrl = await getProfileImageUrl();
+
+    this.setState({
+      profileImageSource: { uri: profileImageUrl }
+    });
   }
+
+  _changeProfilePicture = () => {
+    ImagePicker.showImagePicker(
+      {
+        title: 'Valitse kuva',
+        storageOptions: {
+          skipBackup: true,
+          path: 'images'
+        }
+      },
+      response => {
+        if (response.didCancel) {
+          return;
+        }
+
+        if (response.error) {
+          return;
+        }
+
+        const imageSource = {
+          uri: 'data:image/jpeg;base64,' + response.data
+        };
+
+        // TODO: Send profile pic to server
+        this.setState({ profileImageSource: imageSource });
+      }
+    );
+  };
 
   render() {
     const { componentId } = this.props;
+    const { profileImageSource } = this.state;
 
     return (
       <View style={styles.container}>
@@ -68,7 +107,7 @@ class Profile extends React.Component {
               <FastImage
                 style={styles.image}
                 resizeMode="cover"
-                source={{ uri: getProfileImageUrl() }}
+                source={profileImageSource}
               />
             </TouchableOpacity>
           }
@@ -81,14 +120,15 @@ class Profile extends React.Component {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1
+    flex: 1,
+    backgroundColor: colors.background
   },
   imageContainer: {
     height: 240
   },
   image: {
     flex: 1,
-    marginBottom: -65
+    marginBottom: -70
   },
   tabs: {
     flex: 1
